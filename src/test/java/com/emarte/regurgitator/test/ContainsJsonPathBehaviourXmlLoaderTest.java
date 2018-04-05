2d6c8fe6c8323bc06345447e3a76f34cce5578f6
talmeym <mtalmey@gmail.com>
2018-04-05 20:29:24 +0100

    removed jdom exception from throws - hangover from xalan dependency

--- src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourXmlLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/ContainsJsonPathBehaviourXmlLoaderTest.java
@@ -5,7 +5,6 @@
 package com.emarte.regurgitator.test;
 
 import com.emarte.regurgitator.core.RegurgitatorException;
-import org.dom4j.DocumentException;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
@@ -17,7 +16,7 @@ public class ContainsJsonPathBehaviourXmlLoaderTest extends XmlLoaderTest {
     }
 
     @Test
-    public void testFullLoad() throws DocumentException, SAXException, IOException, RegurgitatorException {
+    public void testFullLoad() throws SAXException, IOException, RegurgitatorException {
         assertExpectationFullLoad("classpath:/ContainsJsonPathBehaviour_fullLoad.xml", "com.emarte.regurgitator.core.Sequence:['regurgitator-configuration-1',[com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'$.something.something',true,com.emarte.regurgitator.extensions.ContainsJsonPathBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]],null]");
     }
 }
